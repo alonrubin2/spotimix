7af7c917be4747cbe4d2506f9354aaa3a5fe1309
alonrubin2 <alonrubin2@gmail.com>
2022-12-11 14:06:11 +0200

    this is stupid, but this is how it works

--- pages/api/auth/getToken.js
+++ pages/api/auth/getToken.js
@@ -2,7 +2,6 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.APP_SECRET;
 
 export default async function handler(req, res) {
-  console.log(new Buffer(`${clientId}":"${clientSecret}`).toString("base64"));
   try {
     const auth = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
@@ -10,7 +9,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization:
           "Basic " +
-          new Buffer.from([`${clientId}":"${clientSecret}`]).toString("base64"),
+          new Buffer(clientId + ":" + clientSecret).toString("base64"),
       },
       body: "grant_type=client_credentials",
       json: true,
