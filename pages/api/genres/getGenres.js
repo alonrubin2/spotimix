9b5a8c475c5ea55a24ea2cfc6bcbd83344662c53
alonrubin2 <alonrubin2@gmail.com>
2022-12-14 12:04:01 +0200

    cleanup

--- pages/api/genres/getGenres.js
+++ pages/api/genres/getGenres.js
@@ -4,12 +4,7 @@ export default async function handler(req, res) {
   const spotifyAPI = new Spotify();
   spotifyAPI.setAccessToken(req.body.token);
   try {
-    // const categories = await spotifyAPI.getCategories({
-    //   country: "US",
-    //   limit: 50,
-    // });
     const genres = await spotifyAPI.getAvailableGenreSeeds();
-    console.log("🚀 ~ file: getCategories.js:9 ~ handler ~ genres", genres);
     return res.send(genres);
   } catch (e) {
     console.error("I AM ERROR ##########", e);
